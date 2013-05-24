13ef16c7348c13ab53a90617db5dd0d3d3730858
root <root@centos6.(none)>
2013-05-24 16:51:15 +0900

    chapter02

---
+++ chapter01/asagao/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asagao::Application.config.secret_token = 'aec86e6fa95c03d336ec5dc7e492382f9ee8a79e8906beab913360fe581e259afa0930cea93b0bd0f3328461850a3d1a6b40179da0031e163cef3b5db76073d3'
